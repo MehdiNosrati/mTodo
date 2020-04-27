2aaafc9966ec23f2de919671a5166cf839eb99df
mehdi <mehdi@qpage.one>
2020-04-27 17:34:48 +0430

    fix identity

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -1,7 +1,7 @@
-const val PUBLISHING_GROUP = "io.mns.base"
+const val PUBLISHING_GROUP = "io.mns.mtodo"
 
 object AppCoordinates {
-    const val APP_ID = "io.mns.base.app"
+    const val APP_ID = "io.mns.base.mtodo"
 
     const val APP_VERSION_NAME = "1.0.0"
     const val APP_VERSION_CODE = 1
