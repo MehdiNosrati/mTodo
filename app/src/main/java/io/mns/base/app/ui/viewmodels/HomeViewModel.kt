2379f4bd9315e124dee036af267e4f50c4e2a055
MnS <nosratimehdi1999@gmail.com>
2020-04-28 22:16:32 +0430

    populate VM with repository actions

--- app/src/main/java/io/mns/base/app/ui/viewmodels/HomeViewModel.kt
+++ app/src/main/java/io/mns/base/app/ui/viewmodels/HomeViewModel.kt
@@ -2,5 +2,30 @@ package io.mns.base.app.ui.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import io.mns.base.app.data.TodoItem
+import io.mns.base.app.data.TodoRepository
+import kotlinx.coroutines.launch
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 
-class HomeViewModel(application: Application): AndroidViewModel(application)
+class HomeViewModel(application: Application): AndroidViewModel(application), KoinComponent {
+    private val repository: TodoRepository by inject()
+
+    fun insert() {
+        viewModelScope.launch {
+            repository.insert(TodoItem("oonee", false))
+        }
+    }
+
+    fun load(): LiveData<List<TodoItem>?> {
+        return repository.load()
+    }
+
+    fun update(item: TodoItem) {
+        viewModelScope.launch {
+            repository.update(item)
+        }
+    }
+}
