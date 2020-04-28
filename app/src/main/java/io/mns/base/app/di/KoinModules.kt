28ed6d1198c10c4cdbc349ec8692c7332342f935
MnS <nosratimehdi1999@gmail.com>
2020-04-28 22:13:25 +0430

    add repository to be injection module

--- app/src/main/java/io/mns/base/app/di/KoinModules.kt
+++ app/src/main/java/io/mns/base/app/di/KoinModules.kt
@@ -1,12 +1,23 @@
 package io.mns.base.app.di
 
+import androidx.room.Room
 import io.mns.androidlib.NotificationUtil
+import io.mns.base.app.data.TodoRepository
+import io.mns.base.app.data.persistence.TodoDataBase
 import org.koin.dsl.module
 
 object KoinModules {
     val appModule = module{
         single {
             NotificationUtil(get())
+        }
+
+        single {
+            Room.databaseBuilder(get(), TodoDataBase::class.java, "todo_db").build()
+        }
+
+        single {
+            TodoRepository(get<TodoDataBase>().todoDao())
         }
     }
 }
