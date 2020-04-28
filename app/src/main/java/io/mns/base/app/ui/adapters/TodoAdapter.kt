202ca83f884b29d84588431f2b114fee207a9d46
MnS <nosratimehdi1999@gmail.com>
2020-04-28 22:15:54 +0430

    add adapter, load data from DB and handle update

---
+++ app/src/main/java/io/mns/base/app/ui/adapters/TodoAdapter.kt
@@ -0,0 +1,88 @@
+package io.mns.base.app.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import io.mns.base.app.data.TodoItem
+import io.mns.base.app.databinding.ItemTodoBinding
+import io.mns.base.app.ui.adapters.callbacks.TodoClickCallBack
+
+class TodoAdapter(private val clickCallBack: TodoClickCallBack) :
+    RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+    private var data: List<TodoItem> = mutableListOf()
+
+    fun setData(nextData: List<TodoItem>) {
+        if (data.isEmpty()) {
+            data = nextData
+            notifyItemRangeInserted(0, data.size)
+        } else {
+            val callback = object : DiffUtil.Callback() {
+                override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+                    data[oldItemPosition].id == nextData[newItemPosition].id
+
+                override fun getOldListSize() = data.size
+
+                override fun getNewListSize() = nextData.size
+
+                override fun areContentsTheSame(
+                    oldItemPosition: Int,
+                    newItemPosition: Int
+                ) = data[oldItemPosition] == nextData[newItemPosition]
+            }
+
+            val result = DiffUtil.calculateDiff(callback)
+            data = nextData
+            result.dispatchUpdatesTo(this)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        return TodoViewHolder.of(LayoutInflater.from(parent.context), parent, clickCallBack)
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemId(position: Int): Long {
+        return data[position].id.hashCode().toLong()
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+
+    class TodoViewHolder(
+        private val binding: ItemTodoBinding,
+        private val clickCallBack: TodoClickCallBack
+    ) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: TodoItem) {
+            binding.item = item
+            binding.callback = clickCallBack
+            binding.done.setOnCheckedChangeListener { _, checked ->
+                clickCallBack.todoChanged(item, checked)
+            }
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun of(
+                inflater: LayoutInflater,
+                parent: ViewGroup,
+                clickCallBack: TodoClickCallBack
+            ): TodoViewHolder {
+                return TodoViewHolder(
+                    ItemTodoBinding.inflate(
+                        inflater,
+                        parent,
+                        false
+                    ), clickCallBack
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/io/mns/base/app/ui/fragments/HomeFragment.kt
+++ app/src/main/java/io/mns/base/app/ui/fragments/HomeFragment.kt
@@ -3,19 +3,42 @@ package io.mns.base.app.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import io.mns.base.app.R
+import io.mns.base.app.data.TodoItem
 import io.mns.base.app.databinding.FragmentHomeBinding
+import io.mns.base.app.ui.adapters.TodoAdapter
+import io.mns.base.app.ui.adapters.callbacks.TodoClickCallBack
 import io.mns.base.app.ui.viewmodels.HomeViewModel
 
 
-class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home), TodoClickCallBack {
     private val viewModel by viewModels<HomeViewModel>()
+    private lateinit var adapter: TodoAdapter
 
-    init {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         loadData()
+        binding.add.setOnClickListener {
+            viewModel.insert()
+        }
     }
 
     private fun loadData() {
+        adapter = TodoAdapter(this)
+        binding.list.setHasFixedSize(true)
+        adapter.setHasStableIds(true)
+        binding.list.adapter = adapter
 
+        viewModel.load().observe(viewLifecycleOwner, Observer {
+            if (it != null) {
+                adapter.setData(it)
+            }
+        })
+    }
+
+    override fun todoChanged(todo: TodoItem, checked: Boolean) {
+        todo.done = checked
+        viewModel.update(todo)
     }
 }
