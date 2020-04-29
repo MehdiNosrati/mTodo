0c855508ce62eb2d45bada478572cf3bc5922325
MnS <nosratimehdi1999@gmail.com>
2020-04-29 23:43:01 +0430

    add bottom nav

--- app/src/main/java/io/mns/base/app/ui/MainActivity.kt
+++ app/src/main/java/io/mns/base/app/ui/MainActivity.kt
@@ -2,12 +2,32 @@ package io.mns.base.app.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import io.mns.base.app.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setBottomNavListener()
+    }
+
+    private fun setBottomNavListener() {
+        bottomBar.onItemSelected = {
+            when (it) {
+                0 -> navController.navigate(R.id.action_doneFragment_to_homeFragment)
+                1 -> navController.navigate(R.id.action_homeFragment_to_doneFragment)
+            }
+        }
+    }
+
+
+    override fun onBackPressed() {
+        navController.navigateUp()
+        bottomBar.setActiveItem(0)
     }
 }
