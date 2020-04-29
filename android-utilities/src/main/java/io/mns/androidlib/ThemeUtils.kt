f7bc2cec985a011fbea56ffd7da16ead7e969233
MnS <nosratimehdi1999@gmail.com>
2020-04-29 18:01:24 +0430

    make toggle theme extension of Resources

--- android-utilities/src/main/java/io/mns/androidlib/ThemeUtils.kt
+++ android-utilities/src/main/java/io/mns/androidlib/ThemeUtils.kt
@@ -4,8 +4,8 @@ import android.content.res.Configuration
 import android.content.res.Resources
 import androidx.appcompat.app.AppCompatDelegate
 
-fun toggleTheme(resources: Resources) {
-    when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+fun Resources.toggleTheme() {
+    when (configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
         Configuration.UI_MODE_NIGHT_NO -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         else -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
