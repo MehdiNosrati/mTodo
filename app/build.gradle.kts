38edab956db02dcf6222eb4310c6fd163f438fa6
MnS <nosratimehdi1999@gmail.com>
2020-04-30 18:49:03 +0430

    enable minify

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,8 @@ android {
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                     getDefaultProguardFile("proguard-android-optimize.txt"),
                     "proguard-rules.pro"
