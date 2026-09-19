74d5c4c4541a37d55fbcf7a9c7be6fa4782ec6d1
Mahdi Nosrati Sahlan <nosratimehdi1999@gmail.com>
2026-09-19 20:35:16 +0100

    build: configure ndk debugSymbolLevel for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,9 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            ndk {
+                debugSymbolLevel = "SYMBOL_TABLE"
+            }
             val releaseSigning = signingConfigs.getByName("release")
             if (releaseSigning.storeFile != null) {
                 signingConfig = releaseSigning
